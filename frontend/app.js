848b4447886f4b28003732805d3cb85aa2fc57c5
AppWeb <leivarobertocarlos@hotmail.com>
2026-08-24 13:36:41 -0300

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 // 1. Configuración de Supabase (Reemplaza con tus datos reales)
-const SUPABASE_URL = 'https://xpufmicxmbhpqocrwgdz.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://xpufmicxmbhpqocrwgdz.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwdWZtaWN4bWJocHFvY3J3Z2R6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1MzE1OTcsImV4cCI6MjEwMzEwNzU5N30.811oNtrlBbEvNvhxaLlvJBZtqSpU98ZQ9sORRh4EIu8';
 
 // Le cambiamos el nombre a 'clienteDb' para que no choque con la librería
